2b822c2fe28b0647ab3b48142b62fd4449a1aebd
vintkhe176898 <vynguyenthikhanh5@gmail.com>
2024-06-18 23:16:01 +0700

    Commit missing SearchStudentController

--- WebApplication1/src/java/Controller/Admin/SearchStudentController.java
+++ WebApplication1/src/java/Controller/Admin/SearchStudentController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller.Admin;
 
 import DAO.StudentClassSessionDBContext;
@@ -23,55 +22,47 @@ import java.util.List;
  *
  * @author NGUYEN THI KHANH VI
  */
-@WebServlet(name="SearchStudentController", urlPatterns={"/search-student"})
+@WebServlet(name = "SearchStudentController", urlPatterns = {"/search-student"})
 public class SearchStudentController extends HttpServlet {
-   
 
     //search by ID and by Name
-   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        // Get the value of the searchInput parameter
-        String searchInput = request.getParameter("searchInput");
 
-        // Call methods to search students by ID and name
-        StudentDBContext sdb = new StudentDBContext();
-        List<Object> combinedResults = new ArrayList<>();
+        String searchInput = request.getParameter("searchInput");
+        StudentClassSessionDBContext sdb = new StudentClassSessionDBContext();
+        List<StudentClassSession> combinedResults = new ArrayList<>();
 
         if (searchInput != null && !searchInput.isEmpty()) {
             try {
                 int id = Integer.parseInt(searchInput);
-                Student studentById = sdb.getStudentById(id);
-                if (studentById != null) {
-                    combinedResults.add(studentById);
+                StudentClassSession studentByID = sdb.getStudentById(id);
+                if (studentByID != null) {
+                    combinedResults.add(studentByID);
                 }
             } catch (NumberFormatException e) {
-                // If searchInput cannot be parsed to an integer, search by name
-                List<Student> studentsByName = sdb.getStudentByName(searchInput);
+                // Search by name
+                List<StudentClassSession> studentsByName = sdb.getStudentByName(searchInput);
                 combinedResults.addAll(studentsByName);
             }
         }
 
-        // Set the search results as an attribute of the request
         request.setAttribute("search", combinedResults);
         request.getRequestDispatcher("FE_Admin/CRUD_Student.jsp").forward(request, response);
     }
 
- 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
-
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
     @Override
     public String getServletInfo() {

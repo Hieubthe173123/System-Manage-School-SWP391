5cf6f9beb69effaaf7f27bac62fd079fd3c19a55
Huydung29 <dungp2746@gmail.com>
2024-07-16 11:25:55 +0700

    Update  DAOLecturers

--- WebApplication1/src/java/DAO/LecturersDBContext.java
+++ WebApplication1/src/java/DAO/LecturersDBContext.java
@@ -5,9 +5,12 @@
 package DAO;
 
 import Entity.Lecturers;
+import Entity.Student;
+import Entity.StudentClassSession;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,41 +20,42 @@ import java.util.logging.Logger;
  */
 public class LecturersDBContext extends DBContext {
 
-    public Lecturers getLecturerByid(int rid) {
-        try {
-            String sql = "SELECT [lid]\n"
-                    + "      ,[lname]\n"
-                    + "      ,[gender]\n"
-                    + "      ,[dob]\n"
-                    + "      ,[phoneNumber]\n"
-                    + "      ,[IDcard]\n"
-                    + "      ,[Address]\n"
-                    + "      ,[Email]\n"
-                    + "      ,[NickName]\n"
-                    + "  FROM [SchoolManagement].[dbo].[Lecturers] Where Lid = ?";
+    public Lecturers getLecturerByid(int lid) {
+    try {
+        String sql = "SELECT [lid]\n"
+                + "      ,[lname]\n"
+                + "      ,[gender]\n"
+                + "      ,[dob]\n"
+                + "      ,[phoneNumber]\n"
+                + "      ,[IDcard]\n"
+                + "      ,[Address]\n"
+                + "      ,[Email]\n"
+                + "      ,[NickName]\n"
+                + "  FROM [SchoolManagement].[dbo].[Lecturers] WHERE [lid] = ?";
 
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setInt(1, rid);
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setInt(1, lid);
 
-            ResultSet rs = stm.executeQuery();
-            if (rs.next()) {
-                Lecturers lec = new Lecturers();
-                lec.setLid(rs.getInt("pid"));
-                lec.setLname(rs.getString("pname"));
-                lec.setGender(rs.getBoolean("gender"));
-                lec.setDob(rs.getString("dob"));
-                lec.setPhoneNumber(rs.getString("phoneNumber"));
-                lec.setIDcard(rs.getString("IDcard"));
-                lec.setEmail(rs.getString("Email"));
-                lec.setAddress(rs.getString("Address"));
-                lec.setNickname(rs.getString("NickName"));
-                return lec;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(RoomDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        ResultSet rs = stm.executeQuery();
+        if (rs.next()) {
+            Lecturers lec = new Lecturers();
+            lec.setLid(rs.getInt("lid"));
+            lec.setLname(rs.getString("lname"));
+            lec.setGender(rs.getBoolean("gender"));
+            lec.setDob(rs.getString("dob"));
+            lec.setPhoneNumber(rs.getString("phoneNumber"));
+            lec.setIDcard(rs.getString("IDcard"));
+            lec.setEmail(rs.getString("Email"));
+            lec.setAddress(rs.getString("Address"));
+            lec.setNickname(rs.getString("NickName"));
+            return lec;
         }
-        return null;
+    } catch (SQLException ex) {
+        Logger.getLogger(LecturersDBContext.class.getName()).log(Level.SEVERE, null, ex);
     }
+    return null;
+}
+
     
     public Lecturers getLecturerByEmail(String email) {
         try {
@@ -88,6 +92,10 @@ public class LecturersDBContext extends DBContext {
         }
         return null;
     }
-    
+    public static void main(String[] args) {
+        LecturersDBContext db = new LecturersDBContext();
+        Lecturers ac = db.getLecturerByid(1);
+        System.out.println(ac);
+    }
    
 }
